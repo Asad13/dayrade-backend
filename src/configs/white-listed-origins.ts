50d7215d841aea28e379a5ffa5ee7e4dcd8796d3
Asad Mujumder <asaduzzaman.anik.me@gmail.com>
2024-05-29 06:39:37 +0600

    Added dashboard subdomain as whitelisted origin

--- src/configs/white-listed-origins.ts
+++ src/configs/white-listed-origins.ts
@@ -1,5 +1,6 @@
 const whiteListedOrigins = [
   /^https:\/\/dashboard.dayrade\.com$/,
+  /^https:\/\/dashboard1.dayrade\.com$/,
   /^https:\/\/(?:[a-z]+\.)?dayrade\.com$/,
   /^https?:\/\/localhost:\d{1,4}/,
 ];
